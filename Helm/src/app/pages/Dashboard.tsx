80056cf0f5c179d568a8ba43f02ae8dd399ac37b
praneethaitharaju <paitharaju@digitalocean.com>
2026-03-15 00:40:32 -0500

    Initial AgileOps setup

--- Helm/src/app/pages/Dashboard.tsx
+++ Helm/src/app/pages/Dashboard.tsx
@@ -847,6 +847,7 @@ export function Dashboard({ currentUser: propsCurrentUser, onLogout: propsOnLogo
               expanded={timelineExpanded}
               onToggle={() => setTimelineExpanded((e) => !e)}
               refreshKey={activityRefreshKey}
+              fallbackTasks={initiatives.map((i) => ({ id: i.id, name: i.name, owner: i.owner }))}
             />
           )}
         </div>

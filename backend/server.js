ebaf78e627fef96ddf45ae69e040557e4347fb5f
praneethaitharaju <paitharaju@digitalocean.com>
2026-03-16 08:52:31 -0500

    Initial AgileOps setup

--- backend/server.js
+++ backend/server.js
@@ -106,7 +106,7 @@ app.use(
     name: 'todo.sid',
     cookie: {
       maxAge: SESSION_MAX_AGE_MS,
-      sameSite: process.env.NODE_ENV === 'production' ? 'lax' : 'lax',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       secure: process.env.NODE_ENV === 'production',
       httpOnly: true,
       path: '/',
